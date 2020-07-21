14f5e81799174caac0f9aad8cbeddbc1cd0d255a
anastasia21112 <anad221122@gmail.com>
2020-07-20 18:48:50 -0600

    modules

---
+++ Python Crash Course/Chapter 8/modules.py
@@ -0,0 +1,3 @@
+from user_profile import build_profile as bp
+
+print(bp('Anastasia', 'Dunca', age='16', focus='Computer Science'))
---
+++ Python Crash Course/Chapter 8/sandwich.py
@@ -0,0 +1,6 @@
+def sandwich(*items):
+    return items
+
+print(sandwich('tomatoes', 'bacon', 'olives', 'cheese'))
+print(sandwich('meat', 'buns'))
+print(sandwich('lettuce'))
